85245c88949a8b6d4a93502c35330ed555cff384
anicdao <amielnicdao@gmail.com>
2019-11-26 15:42:23 -0500

    fixed server routing, added images for matches, hardcoded the choices for matches

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,29 +1,36 @@
 var matches = [
     {
-        name: "Rachael Green",
-        photo: "#",
-        scores: ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"]
+        name: "Rachel Green",
+        photo: "https://e3.365dm.com/19/09/768x432/skynews-rachel-green-friends_4780471.jpg?20190920100815",
+        scores: ["3", "1", "4", "4", "5", "1", "2", "5", "4", "1"]
     },
     {
         name: "Chandler Bing",
-        photo: "#",
-        scores: ["#"]
+        photo: "https://www.stylist.co.uk/images/app/uploads/2017/10/24195115/34fb7869e89b21c70d0662c6af04fb53-828x552.jpg?w=1200&h=1&fit=max&auto=format%2Ccompress",
+        scores: ["4", "2", "5", "5", "1", "2", "3", "1", "5", "2"]
     },
     {
         name: "Monica Geller",
-        photo: "#",
-        scores: ["#"]
+        photo: "https://sites.google.com/site/friends2365/_/rsrc/1472782816715/home/monica-geller/monica.jpg",
+        scores: ["5", "3", "1", "1", "2", "3", "4", "2", "1", "3"]
     },
     {
         name: "Ross Geller",
-        photo: "#",
-        scores: ["#"]
+        photo: "https://vignette.wikia.nocookie.net/friends/images/0/0b/RossGeller.jpg/revision/latest?cb=20180424154547",
+        scores: ["2", "5", "3", "3", "4", "5", "1", "4", "3", "5"]
     },
     {
-        name: "Phoebe Buffet",
-        photo: "#",
-        scores: ["#"]
+        name: "Phoebe Buffay",
+        photo: "https://upload.wikimedia.org/wikipedia/en/thumb/f/f6/Friendsphoebe.jpg/220px-Friendsphoebe.jpg",
+        scores: ["5", "3", "1", "1", "2", "3", "4", "2", "1", "3"]
+    },
+    {
+        name: "Joey Tribbiani",
+        photo: "https://vignette.wikia.nocookie.net/friends/images/f/f5/JoeyTribbiani.jpg/revision/latest?cb=20180424154245",
+        scores: ["1", "2", "2", "5", "3", "2", "5", "1", "2", "2"]
     }
 ]
 
-//export??
+module.exports = matches;
+
+//GOOD
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,21 +1,23 @@
-var friendsData = require("../data/tableData");
+var friendsData = require("../data/friends.js");
 
 module.exports = function(app) {
   app.get("/api/friends", function(req, res) {
     res.json(friendsData);
   });
 
-//   app.post("/api/friends", function(req, res) {
- 
-//     if (tableData.length < 5) {
-//       tableData.push(req.body);
-//       res.json(true);
-//     }
-//     else {
-//       waitListData.push(req.body);
-//       res.json(false);
-//     }
-//   });
+  app.post("/api/friends", function(req, res) { 
+    var totalDifference = 0;
+    var bestMatch = {
+      name: " ",
+      photo: " ",
+      difference: 1000
+    }
+    var userData = body.req;
+    var userName = userData.name;
+    var userScores = userData.scores;
+
+    
+  });
 
 //   app.post("/api/clear", function(req, res) {
 //     // Empty out the arrays of data
