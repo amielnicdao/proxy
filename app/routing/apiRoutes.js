06fbe845674c88b67548b1ada5680930bd923fe7
anicdao <amielnicdao@gmail.com>
2019-11-30 17:28:03 -0500

    added apirputes, htmlroutes, fixed bugs, fixed on submit

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,29 +1,38 @@
-var friendsData = require("../data/friends.js");
+var friendsData = require("../data/friends");
 
-module.exports = function(app) {
-  app.get("/api/friends", function(req, res) {
-    res.json(friendsData);
-  });
+module.exports = function (app) {
+	app.get("/api/friends", function (req, res) {
+		res.json(friendsData);
+	});
 
-  app.post("/api/friends", function(req, res) { 
-    var totalDifference = 0;
-    var bestMatch = {
-      name: " ",
-      photo: " ",
-      difference: 1000
-    }
-    var userData = body.req;
-    var userName = userData.name;
-    var userScores = userData.scores;
+	app.post("/api/friends", function (req, res) {
+		var user = req.body;
+		var userScore = user.scores;
+		var totalDifference;
+		var bestMatch = {
+			name: "",
+			photo: "",
+			difference: 100
+		};
 
-    
-  });
+		for (var i = 0; i < friendsData.length; i++) {
+			totalDifference = 0;
 
-//   app.post("/api/clear", function(req, res) {
-//     // Empty out the arrays of data
-//     tableData.length = 0;
-//     waitListData.length = 0;
+			for (var j = 0; j < friendsData[i].scores.length; j++) {
+				totalDifference += Math.abs(friendsData[i].scores[j] - userScore[j]);
 
-//     res.json({ ok: true });
-//   });
+				if (totalDifference <= bestMatch.difference) {
+					bestMatch.name = friendsData[i].name,
+						bestMatch.photo = friendsData[i].photo,
+						bestMatch.difference = totalDifference
+				}
+			}
+		};
+
+		friendsData.push(user);
+		res.json(bestMatch);
+		console.log(bestMatch);
+	});
 };
+
+
--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 var express = require("express");
-var path = require("path");
 
 var app = express();
 var PORT = process.env.PORT || 8080;
@@ -7,11 +6,11 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static("app/public"));
+
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log("App listening on PORT: " + PORT);
 });
-
-// WORKING
